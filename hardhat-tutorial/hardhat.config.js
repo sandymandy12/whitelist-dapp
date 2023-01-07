a03b8666d09a83d8e51b5bebf73e17dbb871f0c4
Devin Catuns <deecat10@gmail.com>
2023-01-06 19:31:28 -0600

    first commit

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// Contract address - 0xb33a6eE061722E536B4fB64144760Fb964f26917
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
